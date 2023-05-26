67cbc373a4b001d4073d31e55a6179a319e33875
Phoolore <108739763+Phoolore@users.noreply.github.com>
2023-05-26 12:05:16 +0500

    Delete Flask directory
    unnecessary folder

--- Flask/static/scripts/main.js
+++
@@ -1,8 +0,0 @@
-var myHeading = document.querySelector('h1');
-var myButton = document.querySelector('button'); 
-    myButton.onclick = function(){
-    myHeading.textContent = 'Hello, world';}
-    myButton.onclick = function{
-        myHeading.textContent ='Hello'
-
-    }
