ee4bd596bb0a4e1b7b23490fa7511afcfbdf884d
Phoolore <arsennurumanov@gmail.com>
2023-05-30 12:31:23 +0500

    added description for some skills, special pages with details for skills and expanded sidebar

--- app/views.py
+++ app/views.py
@@ -7,14 +7,22 @@ import os
 
 @app.route('/')
 def index_page():
-    df = pd.read_csv("files/skills_relatives.csv")
+    df = pd.read_csv("files/skills_relatives.csv").sort_values(by='Ценность ₽', ascending=False)
     sns.set(style="darkgrid")
-    plt.figure(figsize=(10,4), edgecolor = '0', layout='constrained')
+    plt.figure(figsize=(9.5,4), edgecolor = '0', layout='constrained')
     plot = sns.barplot(data=df, x="Название", y="Ценность ₽", color = '0', width = 0.9)
     fig = plot.get_figure()
     fig.savefig("app/static/table.jpg")
     return render_template('index.html', names= df.iloc[:, 0].to_list(), costs = [round(i,2) for i in df.iloc[:, 1].to_list()], length=len(df))
 
+
+@app.route('/skill/<string:name>')
+def skill_page(name):
+    df = pd.read_csv("files/skill.csv")
+    skill = df.loc[df.name == name].squeeze(axis = 0)
+    length = len(skill)
+    print(type(skill))
+    return render_template('skill.html', skill = skill, length = length)
 
 @app.errorhandler(404)
 def page_not_found(e):
