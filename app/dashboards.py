7667242a96b3df952cf71af9203624c0fb2bdb84
Phoolore <108739763+Phoolore@users.noreply.github.com>
2023-07-01 01:06:10 +0500

    Update dashboards.py Rus lunguage

--- app/dashboards.py
+++ app/dashboards.py
@@ -2,11 +2,12 @@ from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.graph_objects as go
-#from .fullvacparser import parser #for future update
+import locale
+
+locale.setlocale(locale.LC_ALL, 'Russian_Russia')
 
 
-def levelpie(df):#соотношение колличества вакансий по уровням
-    columns = df['level'].unique()
+def levelpie(df):#relationship amount for vacancies of different levels
     df1 = df.groupby('level')['level'].count().rename('Counts').reset_index()
     df1 = df1[df1['level'] != missing]
     labels = df1['level']
@@ -16,8 +17,7 @@ def levelpie(df):#соотношение колличества вакансий
     return fig
 
 
-def dateline(df):#график колличества вакансий по датам загрузки
-    columns = df['published_at'].unique()
+def dateline(df):#amount vacancies for each day
     df1 = df.groupby('published_at')['published_at'].count().rename('Counts').reset_index()
     df1 = df1[df1['published_at'] != missing]
     labels = df1['published_at']
@@ -27,19 +27,24 @@ def dateline(df):#график колличества вакансий по да
     return fig
 
 
-def salbox(df):#зп по уровням
+def salbox(df):#salary for different levels, graph which I want to translate
     df1 = df[df['level'] != missing]
-    fig = go.Figure(data = go.Box(y =df1['level'],  x = (df1['min_salary'] + df1['max_salary']) / 2, orientation='h'))
-    fig.update_layout(width=500, height = 300)
+    fig = []
+    for i in df1['level'].unique():
+        df2 = df1.loc[df1['level'] == i]
+        df2['salary'] = (df2['min_salary'] + df2['max_salary']) / 2
+        fig += [go.Box(y = df2['level'], x = df2['salary'],
+                             orientation='h', name=i)]
+    #fig.update_layout(width=500, height = 300)
     return fig
 
 
 df_keys = pd.read_csv("files/key_words.csv", encoding = 'utf8', delimiter = ';')#данные для запросов для таблицы
 table = [] #хранитель строк таблицы
-target = df_keys.loc[:, :] #Заготовка для будущих фильтров
-missing = 0 #замена для пустых клеток
+target = df_keys.loc[:, :]
+missing = 0 #replace for missing values
 for i, row in target.iterrows():
-    df = pd.read_csv("files/pythonfullvac.csv", encoding = 'utf8', delimiter = ';').fillna(missing) #данные из запроса для аналитики, с заполнеными потерянными данными
+    df = pd.read_csv("files/pythonfullvac.csv", encoding = 'utf8', delimiter = ';').fillna(missing) #information for response with filled missing
     avg_sal = int(((df['min_salary'] + df['min_salary'])/2).mean())
     table += [html.Tr([
             html.Th(["Профессия"], scope="col", style={"width":"11%;"}),
@@ -62,8 +67,13 @@ for i, row in target.iterrows():
             
             html.Th([dcc.Graph(
             id='salbox'+ str(i),
-            figure=salbox(df)
-            )], scope="col", style={"width":"100%;"}),
+            figure={'data' : salbox(df),
+                    'layout' : {
+                        'width' : 500,
+                        'height' : 300,
+                        'locale': 'Russian_Russia'}} )],#here was a try to change language of graph using framework locale, despite the attributes width and high worked locale not
+                    
+            scope="col", style={"width":"100%;"}),
         ])
     ]
 
@@ -75,7 +85,7 @@ dash.layout = dbc.Container([
     dbc.Row([
         dbc.Col([
             html.Table([
-                html.Thead([#название столбцов таблицы
+                html.Thead([#names of columns of table 
                     html.Tr([
                         html.Th(["Тип"], scope="col", style={"width":"11%;"}),
                         html.Th(["Сфера"], scope="col", style={"width":"11%;"}),
@@ -89,7 +99,7 @@ dash.layout = dbc.Container([
                     ])
                     ],
                     className = "thead-dark"),
-                html.Tbody(#строки таблицы
+                html.Tbody(#table's rows
                     table
                     )
                 ], style = {"margin-bottom": 0, "margin-left": 0, "margin-right": "0%", "margin-top": 0}),
