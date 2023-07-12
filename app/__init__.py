432c4bd920e51657496c8d7f70de6aa6a6e2e4e3
Phoolore <arsennurumanov@gmail.com>
2023-07-12 09:42:11 +0500

    added favicon to dash apps

--- app/__init__.py
+++ app/__init__.py
@@ -5,11 +5,17 @@ import pandas as pd
 import plotly.graph_objects as go
 import asyncio
 
-
+# создание главного сервера (app) и страниц с графиками(dash_...)
 app = Flask(__name__)
 app.config.from_object('config.Config')
-dash_table_spec = Dash(__name__, server=app, url_base_pathname='/table_spec/', update_title='Загрузка...', assets_folder = 'app/static')
-dash_table_skill = Dash(__name__, server=app, url_base_pathname='/table_skill/', update_title='Загрузка...', assets_folder = 'app/static')
+dash_table_spec = Dash(__name__, server=app, url_base_pathname='/table_spec/', update_title='Загрузка...', assets_folder = 'static')
+dash_table_skill = Dash(__name__, server=app, url_base_pathname='/table_skill/', update_title='Загрузка...', assets_folder = 'static')
+
+# загрузка favicon для сайтов графиков(внутрение сайты)
+dash_table_spec._favicon = "files/internal_pages_logo.ico"
+dash_table_skill._favicon = "files/internal_pages_logo.ico"
+
+# подключение бд
 db = SQLAlchemy()
 db.init_app(app)
 
